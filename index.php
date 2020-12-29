b85003f71185f82cbb4e0a3a139f69712cd45c38
Clement Ching <lhemon412@outlook.com>
2020-12-29 16:16:11 +0800

    Add comments to /index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
+/*
+-> Authentication System to verify if the user is allowed to access services
+
+-> If success: go to /home/
+-> If failed: go to /error.html
+*/
+
+
 // No information provided
 if (!isset($uid) || !isset($passcode)) {
   header("Location: error.html");
 }
 
+// Initialization
 $uid = $_GET["uid"];
 $passcode = $_GET["pc"];
 
@@ -18,14 +27,13 @@ if ($conn->connect_error) {
   die("Database error: " . $conn->connect_error);
 }
 
+// Requesting data and verify
 $result = $conn->query("SELECT COUNT(*) FROM `notebooks` WHERE `uid`=$uid AND `passcode`='$passcode'");
 if ($result->fetch_assoc()["COUNT(*)"] == "1") {
   session_start();
   $_SESSION["uid"] = $uid;
   header("Location: home");
-  echo "Login successfully";
 } else {
   header("Location: error.html");
 }
-
 ?>
