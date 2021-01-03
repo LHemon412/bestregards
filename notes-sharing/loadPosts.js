caaa8b2d4fcd3756357f34bd31b2ec7dbd587389
Clement Ching <lhemon412@outlook.com>
2021-01-03 22:14:32 +0800

    Flexible image sizes and attachments divs. "...more" indicator for more images

--- notes-sharing/loadPosts.js
+++ notes-sharing/loadPosts.js
@@ -44,32 +44,34 @@ $(() => {
           img.onload = function() {
             let height = this.height;
             let width = this.width;
+            let hwratio = height / width;
             if (this.height > ($(window).height() * 0.6)) {
               height = $(window).height() * 0.6;
-              width = "auto";
+              width = height / hwratio;
               let compress_ratio = ($(window).height() * 0.6) / this.height;
               if ((this.width * compress_ratio) > ($(window).width() * 0.44)) {
                 width = $(window).width() * 0.44;
               }
             } else if (this.width > ($(window).width() * 0.44)){
               width = $(window).width() * 0.44;
-              height = "auto";
+              height = width * hwratio;
+            } else {
+              width /= $(window).width();
+              height /= $(window).height();
             }
 
             let vmin = ($(window).height() > $(window).width()) ? $(window).width() * 0.01 : $(window).height() * 0.01;
-            let mwidth = (width=="auto") ? (this.width * ($(window).height() * 0.6) / this.height + vmin * 2) : (width + $(window).width() * 0.02);
-            let mheight = (height=="auto") ? (this.height * ($(window).width() * 0.44) / this.width + vmin * 2) : (height + $(window).height() * 0.02);
-            $(`div.post#${uid}_${post_id} .post_content .post_attachments`).append(`<div class="atmt_img_container" style="height: ${mheight}px; width: ${mwidth}px"><div class="atmt_img_preview"><img height=${height} width=${width} src="data/${uid}/${post_id}/${main_img}"></div></div>`);
-            $("div.atmt_img_preview img").click(function() {
+            let mwidth = width + 2*vmin;
+            let mheight = height + 2*vmin;
+            $(`div.post#${uid}_${post_id} .post_content .post_attachments`).append(`<div class="atmt_img_container" style="height: ${mheight}px; width: ${mwidth}px"><div class="atmt_img_preview"><img style="height: ${height}px; width=${width}px;" src="data/${uid}/${post_id}/${main_img}"></div></div>`);
+            $("div.atmt_img_container").click(function() {
               $(".modal").css("display", "block");
               $(".modal img").attr("src", `data/${uid}/${post_id}/${main_img}`)
             });
-            $("div.atmt_img_preview img").hover(function() {
-              $(this).css("cursor", "pointer");
-              $(this).animate({opacity: "70%"}, 40);
+            $("div.atmt_img_container").hover(function() {
+              $(".atmt_img_preview img").animate({opacity: "70%"}, 40);
             }, function() {
-              $(this).css("cursor", "auto");
-              $(this).animate({opacity: "100%"}, 40);
+              $(".atmt_img_preview img").animate({opacity: "100%"}, 40);
             });
 
             postData.attachments.forEach((atm, i) => {
@@ -81,11 +83,11 @@ $(() => {
               if (IMAGE_EXT.includes(extension)) {
                 console.log(atm);
                 if ($(".atmt_img_more").length == 0) {
-                  //$(".atmt_img_preview").append(`<span class="atmt_img_more" style="transform: translate(-2vw, ${height * 0.95}px)">...more</span>`);
+                  $(".atmt_img_preview").append(`<span class="atmt_img_more">...more</span>`);
                 }
               } else {
                 if ($(`div.post#${uid}_${post_id} .post_content .post_attachments .atmt_others_container`).length == 0) {
-                  $(`div.post#${uid}_${post_id} .post_content .post_attachments`).append(`<div class="atmt_others_container" style="width: ${$(window).width()*0.878-(mwidth+2*vmin)}px;"></div>`);
+                  $(`div.post#${uid}_${post_id} .post_content .post_attachments`).append(`<div class="atmt_others_container" style="width: ${$(window).width()*0.88 - mwidth - 2*vmin}px;"></div>`);
                 }
                 $(`div.post#${uid}_${post_id} .post_content .post_attachments .atmt_others_container`).append(`<div class="atmt_others" style="order: ${i};"><a href="data/${uid}/${post_id}/${atm}">${atm}</a></div>`);
               }
